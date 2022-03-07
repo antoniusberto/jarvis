c7075af4fb82b27ca414c18b94e5d6d5de4f64fc
Felipe Antonius Berto <felipe.berto@quintoandar.com.br>
2022-03-07 19:25:14 -0300

    flask

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route("/jarvis", methods=["GET", "POST"])
 def jarvis():
     print("entrou hein")
+    return make_response("", 200)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
+    print ("thread finished...exiting")
